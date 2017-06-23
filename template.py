4f872a10572bae4c821674fbbe08a73978a9f665
hblair <hblair@mymail.mines.edu>
2017-06-22 17:25:51 -0700

    Commented out dictionary. Is not used in final.

--- template.py
+++ template.py
@@ -4,8 +4,8 @@ data = '''STRING'''
 
 data = data.replace("	", "    ")
 data = data[:-1]
-rows = data.split('\n')
-ExpNo = {}
-for row in rows:
-    vals = row.split()
-    ExpNo[vals[0]] = (vals[1], vals[2])
+#rows = data.split('\n')
+#ExpNo = {}
+#for row in rows:
+#    vals = row.split()
+#    ExpNo[vals[0]] = (vals[1], vals[2])
